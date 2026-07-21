e509e3b132a789293c00aee229b25446ca09c126
Sandina Rajapaksha <tech.sandina@gmail>
2026-07-22 01:30:02 +0530

    define some data structures

---
+++ types.h
@@ -0,0 +1,40 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// square
+typedef enum {
+    go,
+    special,
+    railway,
+    utility,
+    event,
+    insure,
+    tax,
+    bank,
+    property
+} squareType;
+
+typedef enum {
+    aggresiveInvester,
+    conservativeBanker,
+    riskTaker,
+    opportunisticTrader
+} playerType;
+
+typedef enum { mortgagedToBank, noMortgage } mortgageType;
+
+typedef struct {
+    char *name;
+    squareType type;
+    playerType owner;
+
+    struct {
+        int rental;
+    } utilityProperties;
+
+    struct {
+        mortgageType mortgageStatus;
+    } railwayProperties;
+} square;
+
+#endif
