add9820e2a40b12e3da5b690efafa7865eadaebb
Sandina Rajapaksha <tech.sandina@gmail>
2026-07-29 14:36:54 +0530

    chore: add dummy functions for resolving the square

--- resolveSquare.c
+++ resolveSquare.c
@@ -33,3 +33,11 @@ void resolveSquare(player *player_x, square *board) {
 }
 
 void resolveGO(player *player_x) { player_x->cash = player_x->cash + 2000; }
+void resolveSpecial(player *player_x, square *board) {}
+void resolveRailway(player *player_x, square *board) {}
+void resolveUtility(player *player_x, square *board) {}
+void resolveEvent(player *player_x, square *board) {}
+void resolveInsure(player *player_x, square *board) {}
+void resolveTax(player *player_x, square *board) {}
+void resolveBank(player *player_x, square *board) {}
+void resolveProperty(player *player_x, square *board) {}
