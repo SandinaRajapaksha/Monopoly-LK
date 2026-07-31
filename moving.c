8c3d0d3837f06565354fd9e0446f4c3a8106b8d3
Sandina Rajapaksha <tech.sandina@gmail>
2026-07-31 14:36:50 +0530

    feat : player recieve 2000 by passing go

--- moving.c
+++ moving.c
@@ -1,4 +1,5 @@
 #include "types.h"
+#include <stdio.h>
 void move(player *player_x, square *board) {
     int steps = dice_roller();
     int tempCurrentSquare = player_x->currentSquare;
@@ -11,4 +12,11 @@ void move(player *player_x, square *board) {
     printf("%s moves from Square %d to Square %d\n", player_x->name,
            tempCurrentSquare, board[player_x->currentSquare].squareID);
     resolveSquare(player_x, board);
+    // passed go check
+
+    if ((player_x->currentSquare != 10) && (player_x->currentSquare != 0) &&
+        (player_x->currentSquare < tempCurrentSquare)) {
+        player_x->cash = player_x->cash + 2000;
+        printf("%s received 2000 by passing GO ...\n", player_x->name);
+    }
 }
--- resolveSquare.c
+++ resolveSquare.c
@@ -34,15 +34,19 @@ void resolveSquare(player *player_x, square *board) {
 }
 
 void resolveGO(player *player_x) {
+
     player_x->cash = player_x->cash + 2000;
     printf("%s recieved LKR 2000 by landing on GO ...\n", player_x->name);
 }
 void resolveSpecial(player *player_x, square *board) {
+
     specialityType specialityOfSquare =
         board[player_x->currentSquare].specialityProperties.specililtyOfSquare;
+
     switch (specialityOfSquare) {
     case gotoJail:
-        player_x->currentSquare = 30; // jail square
+        player_x->currentSquare = 10; // jail square
+        player_x->Jail = inside;
         printf("%s got inside the jail ...\n", player_x->name);
         break;
     default:
